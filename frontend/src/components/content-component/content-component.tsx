255c4cd8bc5c02641ac92d9ecf6877f0b90c0601
freddychenyouren2 <freddychenyouren2@gmail.com>
2024-06-23 03:29:24 +0800

    Implement Simple Product Card Template
    This commit is in response of #13
    
    ## Changes made
    - Created a simple Product Card template with MUI as a separate component in `product-card.tsx`
    - Added a simple Welcome Message when users first access the page.

--- frontend/src/components/content-component/content-component.tsx
+++ frontend/src/components/content-component/content-component.tsx
@@ -2,6 +2,7 @@ import './content-component.scss'
 import { Product } from '../../models/response-models'
 import logo from '../../logo.svg'
 import { Grid } from '@mui/material'
+import ProductCard from './product-card'
 
 interface ContentProps {
   products: Product[]
@@ -11,13 +12,22 @@ export function ContentComponent({ products }: ContentProps) {
   return (
     <Grid className="content" container direction="row" spacing={2}>
       {products.length === 0 && (
-        <img src={logo} className="placeholder" alt="logo" />
+        <div className="welcome-message">
+          <img src={logo} className="placeholder" alt="logo" />
+          <h2>Welcome to LostKids.</h2> 
+          <h3>From TikTok, Gifts that Rock!</h3>
+        </div>
       )}
       {products.map((product) => {
         return (
           <Grid item xs={6} key={product.id}>
-            <h3>{product.name}</h3>
-            <p>{product.description}</p>
+            <ProductCard
+            id={product.id}
+            name={product.name}
+            price={product.price}
+            description={product.description}
+            image={product.image}
+          />
           </Grid>
         )
       })}
