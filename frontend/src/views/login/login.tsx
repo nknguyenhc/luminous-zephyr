42064ca717c8b327bcfadcc144b55bd82fad5a42
Arif <arifkhalid99@gmail.com>
2024-06-22 21:34:58 +0800

    Remove redundant error creation

--- frontend/src/views/login/login.tsx
+++ frontend/src/views/login/login.tsx
@@ -7,10 +7,7 @@ export function Login() {
   const user = useAuth()
 
   const handleLogin = useCallback(() => {
-    if (!user) {
-      throw new Error('useAuth must be used within an AuthProvider')
-    }
-    user.logIn('username', 'password')
+    user!.logIn('username', 'password')
   }, [user])
   return (
     <Button className="login-center" variant="contained" onClick={handleLogin}>
