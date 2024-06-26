9405d097dc553211524b35e7549dd2b966242bfa
Singa-pirate <E0969244@u.nus.edu>
2024-06-27 01:21:49 +0800

    Draft authentication APIs

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
+from authlib.integrations.starlette_client import OAuth
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from os import environ as env
+from starlette.config import Config
+from starlette.middleware.sessions import SessionMiddleware
+from starlette.requests import Request
+
+load_dotenv('.env')
 
 app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key=env.get("SECRET_KEY"))
+
+config = Config('.env')
+oauth = OAuth(config)
+oauth.register(
+    'auth0',
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration',
+)
+
 
 app.mount("/static", StaticFiles(directory="./frontend/build/static"), name="static")
 
@@ -17,6 +37,21 @@ def favicon():
 @app.get('/manifest.json')
 def manifest():
     return FileResponse('./frontend/build/manifest.json')
+
+@app.get("/login")
+async def login(request: Request):
+    redirect_uri = request.url_for("test")
+    print(redirect_uri)
+    return await oauth.auth0.authorize_redirect(
+        request,
+        redirect_uri=redirect_uri
+    )
+
+@app.get("/test")
+async def test(request: Request):
+    token = await oauth.auth0.authorize_access_token(request)
+    user = token['userinfo']
+    return user
 
 # Other endpoints
 
