17af460aa341245d9ef127ae48c7a1be7401b2bb
Arif <arifkhalid99@gmail.com>
2024-06-28 16:55:15 +0800

    WIP: Use backend api

--- frontend/src/views/login/login.tsx
+++ frontend/src/views/login/login.tsx
@@ -4,11 +4,18 @@ import { useCallback } from 'react'
 import { useAuth } from '../../context/auth-context'
 
 export function Login() {
-  const user = useAuth()
-
-  const handleLogin = useCallback(() => {
-    user!.logIn('username', 'password')
-  }, [user])
+  //const user = useAuth()
+  const handleLogin = useCallback(async () => {
+    // user!.logIn()
+    try {
+      const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/login`, {
+        method: 'POST',
+        mode: 'no-cors',
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
   return (
     <Button className="login-center" variant="contained" onClick={handleLogin}>
       Login
