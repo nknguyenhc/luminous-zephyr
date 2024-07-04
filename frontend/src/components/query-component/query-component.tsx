1cb03a2c86ce72b941450b026c702170f2f8478e
freddychenyouren2 <freddychenyouren2@gmail.com>
2024-07-04 19:18:42 +0800

    First Attempt of More Inputs
    This commit main objective is to produce an MVP.
    
    - Managed to let the app query categories and price ranges by changing the query type to accept multiple fields.
    - Added some simple error checkings for MVP.
    - Styled Frontend user input for Categories and Price ranges.

--- frontend/src/components/query-component/query-component.tsx
+++ frontend/src/components/query-component/query-component.tsx
@@ -1,21 +1,51 @@
-import { IconButton, Input } from '@mui/material'
+import { FormControl, IconButton, Input, InputLabel, MenuItem, Select, SelectChangeEvent, TextField } from '@mui/material'
 import { ChangeEvent, useState, useCallback } from 'react'
 import './query-component.scss'
 import { Send } from '@mui/icons-material'
 import { useLoading } from '../../context/loading-context'
 
-type QueryComponentProps = {
-  sendQuery: (query: string) => void;
+interface Query {
+  description: string;
+  categories: string[];
+  priceRange: { lower: number; upper: number }
 }
 
+type QueryComponentProps = {
+  sendQuery: (query: Query) => void;
+}
+
+// For MVP. Improvement in performance shall be considered at a later stage.
+const categories = [
+  "Accessories",
+  "Arts & Crafts",
+  "Automotive",
+  "Bathroom Accessories",
+  "Baby Accessories",
+  "Fashion",
+  "Electronics",
+  "Personal Essentials",
+  "Food & Beverages",
+  "Household Items",
+  "Health & Beauty",
+  "Jewelry & Watches",
+  "Kitchenware",
+  "Office Supplies",
+  "Sports & Outdoors",
+  "Stationery",
+  "Toys & Games"
+];
+
 export function QueryComponent({ sendQuery }: QueryComponentProps) {
-  const [query, setQuery] = useState('')
+  const [description, setDescription] = useState('');
+  const [category, setCategory] = useState(''); // KEEP IN VIEW
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+  const [priceRange, setPriceRange] = useState({ lower: 0, upper: 9999 });
   const { setLoading } = useLoading();
 
   const handleClick = useCallback(
-    (input: string) => {
+    (query: Query) => {
       setLoading(true)
-      sendQuery(input)
+      sendQuery(query)
     },
     [sendQuery, setLoading]
   )
@@ -24,19 +54,61 @@ export function QueryComponent({ sendQuery }: QueryComponentProps) {
     <form
       onSubmit={(e) => {
         e.preventDefault()
-        handleClick(query)
+        handleClick({ description, categories: selectedCategories, priceRange })
       }}
       className="query-container"
     >
       <Input
         className="query-input"
-        value={query}
+        value={description}
         onChange={(event: ChangeEvent<HTMLInputElement>) => {
-          setQuery(event.target.value)
+          setDescription(event.target.value)
         }}
         placeholder="Describe the product"
         disableUnderline={true}
       ></Input>
+
+      <div className="query-row">
+        <FormControl>
+          <InputLabel>Category</InputLabel>
+          <Select
+            multiple
+            value={selectedCategories}
+            onChange={(event: SelectChangeEvent<string[]>) => {
+              setSelectedCategories(event.target.value as string[]);
+            }}
+            className='query-category-select'
+          >
+
+          {categories.map((category) => (
+            <MenuItem key={category} value={category}>
+              {category}
+            </MenuItem>
+          ))}
+            
+          </Select>
+        </FormControl>
+
+        <TextField
+          className="query-price"
+          label="Min Price"
+          type="number"
+          value={priceRange.lower}
+          onChange={(event: ChangeEvent<HTMLInputElement>) => {
+            setPriceRange({ ...priceRange, lower: parseInt(event.target.value, 10) || 0});
+          }}
+        />
+        <TextField
+          className="query-price"
+          label="Max Price"
+          type="number"
+          value={priceRange.upper}
+          onChange={(event: ChangeEvent<HTMLInputElement>) => {
+            setPriceRange({ ...priceRange, upper: parseInt(event.target.value, 10) || 0});
+          }}
+        />
+      </div>
+      
       <IconButton
         type="submit"
         className="query-send-icon-button"
--- frontend/src/hooks/useProducts.ts
+++ frontend/src/hooks/useProducts.ts
@@ -2,17 +2,46 @@ import { useState, useCallback } from 'react'
 import { Product } from '../models/response-models'
 import { useLoading } from '../context/loading-context'
 
+interface Query {
+  description: string;
+  categories: string[];
+  priceRange: { lower: number; upper: number }
+}
+
 export function useProducts() {
   const [products, setProducts] = useState([] as Product[])
   const { setLoading } = useLoading()
 
   const sendQuery = useCallback(
-    async (query: string) => {
-      if (!query) {
+    async (query: Query) => {
+      console.log("Query Category: ", query.categories)
+      console.log("Query Price Range: ", query.priceRange)
+      
+      // Error Handling: Description
+      if (!query.description) {
         //TODO add error handling
+        alert("Please describe your gift! Thank you!")
+        setLoading(false);
         return
       }
 
+      // Error Handling: Category Selection
+      if (query.categories.length === 0) {
+        // No categories selected. For now this is not allowed by the Prompt BaseModel.
+        // Must have at least one category selected.
+        alert("Please choose at least one category. Thank you!")
+        setLoading(false);
+        return
+      }
+
+      // Error Handling: Price Range Input
+      if (query.priceRange.lower > query.priceRange.upper) {
+        alert ("Minimum Price must be lower than Maximum Price!")
+        setLoading(false);
+        return
+      }
+
+      console.log("Loading spinner called.")
       setLoading(true)
       try {
         const response = await fetch('/api/prompt', {
@@ -22,7 +51,9 @@ export function useProducts() {
           },
           credentials: 'include',
           body: JSON.stringify({
-            prompt: query,
+            prompt: query.description,
+            categories: query.categories,
+            price_range: query.priceRange,
           }),
         })
         const data = await response.json()
